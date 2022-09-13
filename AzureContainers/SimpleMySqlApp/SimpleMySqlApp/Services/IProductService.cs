c8e0e266c78b6fa6495071726945532a97b4e5b2
ap-f12 <ap@f12.email>
2022-09-13 16:33:48 +1200

    Container registry and container groups deployed with arm templates

---
+++ AzureContainers/SimpleMySqlApp/SimpleMySqlApp/Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using SimpleMySqlApp.Model;
+
+namespace SimpleMySqlApp.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
---
+++ AzureContainers/SimpleMySqlApp/SimpleMySqlApp/Services/ProductService.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using SimpleMySqlApp.Model;
+
+namespace SimpleMySqlApp.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly string _connectionString;
+        public ProductService(IConfiguration configuration)
+        {
+            _connectionString = configuration["ConnectionStrings:DefaultConnection"];
+        }
+        public List<Product> GetProducts()
+            
+        {
+            var products = new List<Product>();
+            try
+            {
+                using (var connection = new MySqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = "SELECT * FROM Products";
+                    var reader = command.ExecuteReader();
+                    
+                    while (reader.Read())
+                    {
+                        var product = new Product
+                        {
+                            ProductID = reader.GetInt32(0),
+                            ProductName = reader.GetString(1),
+                            Quantity = reader.GetInt32(2)
+                        };
+                        products.Add(product);
+                    }
+                    connection.Close();
+                    return products;
+                }
+            }
+            catch(Exception ex)
+            {
+                var product = new Product
+                {
+                    ProductID = 0,
+                    ProductName = "empty",
+                    Quantity = 0
+                };
+                products.Add(product);
+                return products;
+
+            }
+           
+        }
+    }
+ 
+}
