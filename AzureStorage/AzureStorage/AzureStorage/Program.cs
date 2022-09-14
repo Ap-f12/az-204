4ee5dea3b43487809cb0ee551b2b7ba8040a9fa0
ap-f12 <ap@f12.email>
2022-09-14 16:58:21 +1200

    storage account created with arm template

---
+++ AzureStorage/AzureStorage/AzureStorage/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+var connectionString = config["ConnectionStrings:DefaultConnection"];
+var containerName = "containerfromconsoleapp";
+var blobName = "testUpload.html";
+var blobPath = "C:\\Users\\F12\\source\\repos\\az-204\\AzureStorage\\testupload.html";
+Console.WriteLine(blobPath);
+Console.ReadLine();
+var downloadPath = "./";
+
+var blobServiceClient = new BlobServiceClient(connectionString);
+await blobServiceClient.CreateBlobContainerAsync(containerName);
+Console.WriteLine("container created");
+Console.ReadLine();
+
+var blobContainerClient = new BlobContainerClient(connectionString, containerName);
+var blobClient = blobContainerClient.GetBlobClient(blobName);
+await blobClient.UploadAsync(blobPath, true);
+Console.WriteLine("blob uploaded");
+Console.ReadLine();
+
+await foreach(var item in blobContainerClient.GetBlobsAsync())
+{
+    var contentLength = item.Properties.ContentLength;
+    Console.WriteLine("Content length of the blob is {0}", contentLength);
+    
+}
+Console.ReadLine();
+
+
+await blobClient.DownloadToAsync(blobPath);
+Console.WriteLine("blob downloaded");
+Console.ReadLine();
+
+var metaData = new Dictionary<string, string>();
+metaData.Add("testKey", "testValue");
+
+await blobClient.SetMetadataAsync(metaData);
+Console.WriteLine("metadata set");
+Console.ReadLine();
+
+BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+
+foreach( var md in blobProperties.Metadata)
+{
+    Console.WriteLine("The metadata key and values is {0} and {1}", md.Key, md.Value);
+}
+Console.ReadLine();
+
+BlobLeaseClient blobLeaseClient = blobClient.GetBlobLeaseClient();
+var timeSpan = new TimeSpan(0, 0, 1, 00);
+var response = await blobLeaseClient.AcquireAsync(timeSpan);
+Console.WriteLine("lease acquired and lease id is {0}", response.Value.LeaseId);
+
+Console.ReadLine();
+
+
+
---
+++ AzureStorage/AzureStorage/AzureStorageTable/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using Azure;
+using Azure.Data.Tables;
+using Microsoft.Extensions.Configuration;
+using System.Runtime.CompilerServices;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+var connSting = config["ConnectionStrings:DefaultConnection"];
+var tableName = "Products";
+Console.WriteLine(connSting);
+Console.ReadLine();
+
+TableClient tableClient = new TableClient(connSting, tableName);
+
+
+
+async Task AddEntitiesToTable(string orderId, string category, int quantity)
+{
+    var tableEntity = new TableEntity(category, orderId)
+    {
+        {"quantity", quantity }
+    };
+  await   tableClient.AddEntityAsync(tableEntity);
+}
+
+
+await AddEntitiesToTable("01", "laptops", 200);
+await AddEntitiesToTable("02", "Mobiles", 200);
+await AddEntitiesToTable("03", "Desktops", 200);
+await AddEntitiesToTable("04", "laptops", 200);
+await AddEntitiesToTable("05", "laptops", 200);
+
+Console.WriteLine("table rows added");
+Console.ReadLine();
+
+
+Pageable<TableEntity> results = tableClient.Query<TableEntity>(entity => entity.PartitionKey == "laptops");
+foreach(TableEntity tableEntity in results)
+{
+    Console.WriteLine("Row key is {0}",tableEntity.RowKey);
+    Console.WriteLine("Quantity is {0}", tableEntity.GetInt32("quantity"));
+}
+
+Console.ReadLine();
+await tableClient.DeleteEntityAsync("laptops", "01");
+Console.WriteLine("row deleted");
+Console.ReadLine();
+
+
+var updatedTableEntity = new TableEntity("laptops", "05")
+    {
+        {"quantity", 500 }
+    };
+Console.WriteLine("row updated");
+Console.ReadLine();
+
+
+await tableClient.UpdateEntityAsync<TableEntity>(updatedTableEntity, ETag.All, TableUpdateMode.Replace);
+Pageable<TableEntity> updatedResults = tableClient.Query<TableEntity>(entity => entity.PartitionKey == "laptops");
+
+foreach (TableEntity tableEntity in updatedResults)
+{
+    Console.WriteLine("Row key is {0}", tableEntity.RowKey);
+    Console.WriteLine("Quantity is {0}", tableEntity.GetInt32("quantity"));
+}
+
+Console.ReadLine();
